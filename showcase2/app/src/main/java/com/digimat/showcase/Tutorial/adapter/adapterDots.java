869cf063204958eca5014aced07ebc8b02e9e528
DIGIMAT <36770624+DIGIMATWEB@users.noreply.github.com>
2025-10-29 20:04:10 -0600

    update scroll

--- showcase2/app/src/main/java/com/digimat/showcase/Tutorial/adapter/adapterDots.java
+++ showcase2/app/src/main/java/com/digimat/showcase/Tutorial/adapter/adapterDots.java
@@ -1,4 +1,71 @@
 package com.digimat.showcase.Tutorial.adapter;
 
-public class adapterDots {
-}
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+import com.digimat.showcase.R;
+
+public class adapterDots extends RecyclerView.Adapter<adapterDots.ViewHolder> {
+
+    private Context context;
+    private Integer postionDot;
+
+
+    public adapterDots(Integer postionDot, Context context) {
+        this.context = context;
+        this.postionDot=postionDot;
+    }
+
+    @NonNull
+    @Override
+    public adapterDots.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dots_pager, parent, false);
+        return new adapterDots.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull adapterDots.ViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+        ViewGroup.LayoutParams params = holder.dot.getLayoutParams();
+
+        // Convert dp to px
+        int widthActive = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 12, context.getResources().getDisplayMetrics());
+        int widthInactive = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 6, context.getResources().getDisplayMetrics());
+
+        // Apply width based on selected position
+        params.width = (position == postionDot) ? widthActive : widthInactive;
+        holder.dot.setLayoutParams(params);
+
+        // Optional: change color too if you want
+        int color = (position == postionDot) ? Color.BLACK : Color.parseColor("#C8C8C8");
+        ((CardView) holder.dot).setCardBackgroundColor(color);
+    }
+    @Override
+    public int getItemCount() {
+        return 5    ;
+    }
+
+    public void notifyNext(Integer postionDot) {
+        this.postionDot=postionDot;
+        notifyDataSetChanged();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private CardView dot;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            dot= itemView.findViewById(R.id.dot);
+        }
+    }
+    }
--- showcase2/app/src/main/java/com/digimat/showcase/Tutorial/view/Tutorial.java
+++ showcase2/app/src/main/java/com/digimat/showcase/Tutorial/view/Tutorial.java
@@ -7,15 +7,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
+import com.digimat.showcase.Mas.view.masFrament;
 import com.digimat.showcase.Menu.view.layoutInteface;
+import com.digimat.showcase.Profile.adapter.adapterProfile;
 import com.digimat.showcase.R;
 import com.digimat.showcase.Tutorial.adapter.AdapterHelpV2;
+import com.digimat.showcase.Tutorial.adapter.adapterDots;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +34,8 @@ public class Tutorial extends Fragment implements View.OnClickListener {
     private AdapterHelpV2 adapterViewPager;
     private RecyclerView imgvStep;
     private int positionCurrentItem = 0;
+    private adapterDots adapter;
+    private Integer postionDot=0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,6 +48,8 @@ public class Tutorial extends Fragment implements View.OnClickListener {
     private void initView(View view) {
         viewPager = view.findViewById(R.id.view_pager_loginhelp);
         imgvStep = view.findViewById(R.id.imgvStep);
+
+
         TextView btnSkip = view.findViewById(R.id.txt_btn_skip);
         TextView btnNext = view.findViewById(R.id.txt_btn_next);
 
@@ -68,21 +79,43 @@ public class Tutorial extends Fragment implements View.OnClickListener {
 
             }
         });
+        fillSizeDots();
 
+    }
+
+    private void fillSizeDots() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        imgvStep.setLayoutManager(layoutManager);
+        adapter=new adapterDots(postionDot,getContext());
+        imgvStep.setAdapter(adapter);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.txt_btn_next:
-                if (positionCurrentItem == 8) {
-                    getActivity().onBackPressed();
+                if (postionDot< adapter.getItemCount()-1 ) {
+                    postionDot=postionDot+1;
+                    adapter.notifyNext(postionDot);
                 } else {
-                    viewPager.setCurrentItem(positionCurrentItem + 1, true);
+                    Toast.makeText(getContext(), "Limite superior excedido", Toast.LENGTH_SHORT).show();
                 }
                 break;
             case R.id.txt_btn_skip:
-                getActivity().onBackPressed();
+                if(postionDot!=0){
+                    if(postionDot>0){
+                        postionDot=postionDot-1;
+                        adapter.notifyNext(postionDot);
+                    }else{
+                        Toast.makeText(getContext(), "Limite inferior excedido", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    FragmentManager manager = getActivity().getSupportFragmentManager();
+                    FragmentTransaction transaction = manager.beginTransaction();
+                    masFrament moreOptions = new masFrament();
+                    transaction.replace(R.id.conteinerMainFragments, moreOptions, masFrament.TAG).commit();
+                }
+
                 break;
         }
     }
